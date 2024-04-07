2006e9d85e56c1b0ffa665418df941c750d0338e
Levy <100963276+4levy@users.noreply.github.com>
2024-04-07 15:55:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const { launcher } = require("@loybung/launcher");
 const path = require("path");
 
+app.get('/', (req, res) => {
+  const imagePath = path.join(__dirname, 'index.html');
+  res.sendFile(imagePath);
+});
+
+app.listen(port, () => {
+  console.log(`🔗 Listening to port : http://localhost:${port}`);
+});
+
 const options = {
   url: "https://loybung.vercel.app/api/project/selfbot",
   filepath: path.resolve(__dirname, "app.js"),
